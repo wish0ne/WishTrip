4881b3e7781b1a375e9dad6a1c65700d1138c1bd
wish0ne <wish@khu.ac.kr>
2022-08-16 20:13:09 +0900

    style: 마이페이지 미리보기 추가(WISHTRIP-30)

---
+++ src/pages/Mypage/components/Content.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+const StyledContent = styled.div`
+  position: relative;
+  img {
+    width: 15.2rem;
+    height: 15.2rem;
+    object-fit: cover;
+    border-radius: 0.8rem;
+  }
+  div {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    bottom: 1.2rem;
+    left: 1.2rem;
+    span {
+      font-family: "ExtraBold";
+      color: white;
+      font-size: 1.3rem;
+    }
+    span + span {
+      font-family: "Medium";
+      font-size: 1.2rem;
+    }
+  }
+`;
+
+interface ContentPropsType {
+  image: string;
+  title: string;
+  user: string;
+}
+
+function Content({ image, title, user }: ContentPropsType) {
+  return (
+    <StyledContent>
+      <img src={image} />
+      <div>
+        <span>{title}</span>
+        <span>{user}</span>
+      </div>
+    </StyledContent>
+  );
+}
+
+export default Content;
